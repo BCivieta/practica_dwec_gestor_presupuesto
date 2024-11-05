ba22f1c6d121952e01c6d2b5537c79fd7e670b71
Blanca Civieta <blancivi@gmail.com>
2024-11-05 12:10:19 +0100

    modificación de export y definición de funciones vacías

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -90,7 +90,12 @@ function calcularBalance(){
     let balance= presupuesto - calcularTotalGastos();
     return balance;
 }
+function filtrarGastos(){
 
+}
+function agruparGastos(){
+
+}
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
@@ -103,5 +108,7 @@ export   {
     anyadirGasto,
     borrarGasto,
     calcularTotalGastos,
-    calcularBalance
+    calcularBalance,
+    filtrarGastos,
+    agruparGastos,
 }
